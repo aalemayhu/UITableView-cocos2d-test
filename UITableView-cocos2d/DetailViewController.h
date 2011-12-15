38213d411fdb4452ba27eaa2c3ca99bac21cac56
Alexander Alemayhu <alexander.alemayhu@googlemail.com>
2011-12-15 23:27:14 +0100

    Detail viewcontroller.

---
+++ UITableView-cocos2d/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  UITableView-cocos2d
+//
+//  Created by Alexander Alemayhu on 15.12.11.
+//  Copyright (c) 2011 Flexnor. All rights reserved.
+//
+
+
+
+@interface DetailViewController : UIViewController{
+    
+    NSString *title;
+}
+
+-(id) initWitTitle:(NSString *)aTitle;
+@end
---
+++ UITableView-cocos2d/DetailViewController.m
@@ -0,0 +1,67 @@
+//
+//  DetailViewController.m
+//  UITableView-cocos2d
+//
+//  Created by Alexander Alemayhu on 15.12.11.
+//  Copyright (c) 2011 Flexnor. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@implementation DetailViewController
+
+
+-(id) initWitTitle:(NSString *)aTitle{
+    
+    if (self = [super init]) {
+        title = [aTitle copy];
+    }
+    
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [self setTitle:title];
+    [super viewDidLoad];
+}
+
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+-(void) dealloc{
+    
+    [title release];
+    [super dealloc];
+}
+
+@end
